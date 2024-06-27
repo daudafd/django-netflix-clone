bf4694b3cd1402e431c5d381eaa1a3b046587d3c
daudafd <daudafd@gmail.com>
2024-06-27 07:27:30 +0100

    adjusted SECRET KEY

--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=9vhq#r#v7^umx=n3b8$g_6*v#7ejsm)lz=@k@&gawobmbjqmv'
+# SECRET_KEY = 'django-insecure-=9vhq#r#v7^umx=n3b8$g_6*v#7ejsm)lz=@k@&gawobmbjqmv'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-=9vhq#r#v7^umx=n3b8$g_6*v#7ejsm)lz=@k@&gawobmbjqmv')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
